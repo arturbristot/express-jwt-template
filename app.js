bdafb5aa05caa034ad603226f01adb88dd787e35
Artur Bristot <arturbristot123@gmail.com>
2025-10-04 17:33:48 -0300

    cors

--- app.js
+++ app.js
@@ -3,13 +3,20 @@ dotenv.config();
 
 import express from 'express';
 import cookieParser from "cookie-parser";
+import cors from 'cors';
 
 import authRoutes from "./src/modules/auth/routes/authRoutes.js";
 import userRoutes from "./src/modules/users/routes/userRoutes.js";
-//import messagesRoutes from "./src/modules/messages/routes/messagesRoutes.js";
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.use(cors({
+  origin: 'http://localhost:3000', // ou ['http://localhost:3000', 'http://localhost:3001'] //local do front end
+  credentials: true, 
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use(express.json());
 app.use(cookieParser());
